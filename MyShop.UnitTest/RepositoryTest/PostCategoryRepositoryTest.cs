da9e98a40c851a03141b40108f19122c602793fa
autumn_prince <nguyenquangminh2912@gmail.com>
2020-01-30 14:27:30 +0700

    Unit 10 : UnitTest - Repository & Services

---
+++ MyShop.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Data.Infrastructer;
+using MyShop.Data.Repositories;
+using MyShop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        IDbFactory dbFactory;
+        IPostCategoryRepository objRepository;
+        IUnitOfWork unitOfWork;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            //Initialize : đây là phương thức khới chạy đầu tiên.
+            //Khởi tạo 3 phương thức này trước.
+            dbFactory = new DBFactory();
+            objRepository = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            var list =  objRepository.GetAll().ToList();
+            Assert.AreEqual(3, list.Count);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory category = new PostCategory();
+            category.Name = "Test category";
+            category.Alias = "Test-category";
+            category.Status = true;
+
+            var result = objRepository.Add(category);
+            unitOfWork.Commit();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.ID);
+        }
+
+    }
+}
